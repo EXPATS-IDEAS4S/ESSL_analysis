71ba2e3ffeed35777e575e744253fa1dc8bc07e1
Daniele-Corradini <danielecorradini1995@gmail.com>
2023-10-13 18:00:21 +0200

    improving color coded and style

--- code/essl_analysis_functions.py
+++ code/essl_analysis_functions.py
@@ -10,6 +10,11 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.patches as mpatches
 import rasterio
+import matplotlib.colors as mcolors
+import matplotlib.patches as mpatches
+from matplotlib.lines import Line2D
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import cartopy.io.shapereader as shpreader
 
 
 
@@ -60,24 +65,45 @@ def plot_events_essl(data, domain, intensity):
     plt.show()
 
 
-def plot_events_paula(datasets, main_domain, subdomains, intensity, raster_path):
+def plot_events_paula(datasets, main_domain, subdomains, intensity, raster_filename, title, path_file, path_figs):
 
     # Extract main domain boundaries
     minlat_main, maxlat_main, minlon_main, maxlon_main = main_domain
 
-    with rasterio.open(raster_path) as src:
+    with rasterio.open(path_file+raster_filename) as src:
         # Create a map plot
         fig, ax = plt.subplots(figsize=(12, 8),subplot_kw={'projection': ccrs.PlateCarree()})
         ax.set_extent([minlon_main, maxlon_main, minlat_main, maxlat_main])
         ax.add_feature(cfeature.BORDERS, linestyle=':')
         ax.add_feature(cfeature.COASTLINE)
-        ax.add_feature(cfeature.LAKES, alpha=0.5)
+        ax.add_feature(cfeature.LAKES)
         ax.add_feature(cfeature.RIVERS)
 
         extent = [src.bounds.left, src.bounds.right, src.bounds.bottom, src.bounds.top]
-        ax.imshow(src.read(1), origin='upper', cmap='gist_earth', extent=extent, transform=ccrs.PlateCarree(), alpha=0.5)
-
+        ax.imshow(src.read(1), origin='upper', cmap='gist_earth', extent=extent, transform=ccrs.PlateCarree(), alpha=0.5, interpolation='spline36')
+        
         ax.add_feature(cfeature.OCEAN, color='blue')
+
+        # Add lat-lon axis tick labels
+        gl = ax.gridlines(draw_labels=True, linewidth=0.5, color='gray', alpha=0.5, linestyle='--')
+        gl.top_labels = False
+        gl.right_labels = False
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+
+        # # Get major cities and plot them separately
+        # shapename = 'populated_places'
+        # cities = shpreader.Reader(shpreader.natural_earth(resolution='50m',
+        #                                                 category='cultural', name=shapename))
+        # for city in cities.geometries():
+        #     ax.plot(city.x, city.y, '.', color='black', markersize=5)
+
+        # # Add physical labels (like mountain ranges)
+        # physical_labels = cfeature.NaturalEarthFeature(category='cultural', 
+        #                                             name='physical_labels', 
+        #                                             scale='50m', 
+        #                                             facecolor='none')
+        # ax.add_feature(physical_labels, edgecolor='gray')
 
         #set up labels
         num_subdomains = len(subdomains)
@@ -104,6 +130,24 @@ def plot_events_paula(datasets, main_domain, subdomains, intensity, raster_path)
             # get hail events
             hail_data = filtered_data[filtered_data['mode'] == 'hail']
 
+            # Convert 'datetime' column to datetime type
+            rain_data.loc[:, 'datetime'] = pd.to_datetime(rain_data['datetime'])
+            hail_data.loc[:, 'datetime'] = pd.to_datetime(hail_data['datetime'])
+            #rain_data['datetime'] = pd.to_datetime(rain_data['datetime'])
+            #hail_data['datetime'] = pd.to_datetime(hail_data['datetime'])
+
+            # Extract month
+            rain_data['month'] = rain_data['datetime'].dt.month
+            hail_data['month'] = hail_data['datetime'].dt.month
+
+            # Map each month to a color using a colormap
+            colors_list = list(mcolors.TABLEAU_COLORS.values())  # use any colormap you prefer
+            colors_list = ['#0048BA', '#AF002A', '#3B7A57', '#FF7E00', '#BA7890'] 
+            month_to_color = {month: colors_list[month % len(colors_list)] for month in range(1, 13)}
+
+            rain_colors = rain_data['month'].map(month_to_color).tolist()
+            hail_colors = hail_data['month'].map(month_to_color).tolist()
+
             if intensity:
                 sc1 = ax.scatter(rain_data['lon'], rain_data['lat'], 
                         c=rain_data['mm'], marker='o', label=label_rain, 
@@ -114,16 +158,39 @@ def plot_events_paula(datasets, main_domain, subdomains, intensity, raster_path)
                         cmap='Reds', transform=ccrs.PlateCarree())
             else:
                 ax.scatter(rain_data['lon'], rain_data['lat'], 
-                        marker='o', label=label_rain, c='Blue',
-                        transform=ccrs.PlateCarree())
-                
-                ax.scatter(hail_data['lon'], hail_data['lat'], 
-                        marker='^', label=label_hail, c='Red',
-                        transform=ccrs.PlateCarree())
-        
+                marker='o', label='Rain', c=rain_colors,
+                transform=ccrs.PlateCarree())
 
-        # Adjust legend location
-        ax.legend(loc='upper left')#, bbox_to_anchor=(1, 0.5))
+                ax.scatter(hail_data['lon'], hail_data['lat'], 
+                marker='^', label='Hail', c=hail_colors,
+                transform=ccrs.PlateCarree())
+        
+        # Get unique months present in the dataframes
+        unique_rain_months = rain_data['month'].unique()
+        unique_hail_months = hail_data['month'].unique()
+
+        # Combine and deduplicate
+        unique_months = set(unique_rain_months).union(set(unique_hail_months))
+
+        # Custom legend handles for rain and hail markers
+        rain_legend = Line2D([0], [0], marker='o', color='w', markerfacecolor='gray', markersize=10, label='Rain')
+        hail_legend = Line2D([0], [0], marker='^', color='w', markerfacecolor='gray', markersize=10, label='Hail')
+
+        # Custom legend handles for the unique months
+        month_labels = [
+            "January", "February", "March", "April", "May", "June", 
+            "July", "August", "September", "October", "November", "December"
+        ]
+
+        month_handles = [mpatches.Patch(color=month_to_color[month], label=month_labels[month-1]) 
+                        for month in unique_months]
+
+        # Combine handles and labels
+        handles = [rain_legend, hail_legend] + month_handles
+        labels = [h.get_label() for h in handles]
+
+        # Display the combined legend outside the plot on the central right
+        ax.legend(handles=handles, labels=labels, loc='center left', bbox_to_anchor=(1, 0.5))
 
         # Show colorbars at the bottom if intensity is True
         if intensity:
@@ -136,3 +203,4 @@ def plot_events_paula(datasets, main_domain, subdomains, intensity, raster_path)
         # Adjust layout and show the plot
         plt.tight_layout()
         plt.show()
+        fig.savefig(path_figs+title+'.png',bbox_inches='tight')
--- code/essl_analysis_run.py
+++ code/essl_analysis_run.py
@@ -16,7 +16,6 @@ Image.MAX_IMAGE_PIXELS = None  # Disable the limit
 
 # define paths
 path_file = '/home/daniele/Documenti/PhD_Cologne/TeamX/data/'
-raster_file_path = path_file+'NE1_HR_LC_SR_W_DR/NE1_HR_LC_SR_W_DR.tif'
 
 #define filenames
 filename_essl = path_file+'ESWD_HAIL_PRECIP_TORNADO_WIND_5-16_42-51_5_20230101-20231010_v1_6.csv'
@@ -30,12 +29,26 @@ data_paula_grey = pd.read_csv(filename_paula_grey)
 
 # define domain of interest # [minlat, maxlat, minlon, maxlon]
 domain_grey = [45.6, 46.6, 10.7, 11.5] #TeamX domain (Grey domain)
-domain_red = [45.40, 46.95, 11, 11.81] #TeamX domain (Red domain)
+domain_red = [46.40, 46.95, 11, 11.81] #TeamX domain (Red domain) NB: minlat is 46.40 and not 45.40 as reported in filename
 domain_expats = [42, 51.5, 5, 16] #EXPATS domain
+
+# Padding value
+padding = 0.2
+
+# Determine encompassing domain
+left_border = min(domain_grey[0], domain_red[0]) - padding
+right_border = max(domain_grey[1], domain_red[1]) + padding
+bottom_border = min(domain_grey[2], domain_red[2]) - padding
+top_border = max(domain_grey[3], domain_red[3]) + padding
+
+encompassing_domain = [left_border, right_border, bottom_border, top_border]
 
 # plot the spatia distribution of the events
 #essl_analysis_functions.plot_events_essl(data_essl, domain_large, False)
-essl_analysis_functions.plot_events_paula([data_paula_red,data_paula_grey], domain_expats, [domain_red,domain_grey], False, raster_file_path)
+#essl_analysis_functions.plot_events_paula([data_paula_red,data_paula_grey], domain_expats, [domain_red,domain_grey], False, raster_file_path)
+raster_filename = 'NE1_HR_LC_SR_W_DR/NE1_HR_LC_SR_W_DR.tif'
+title = 'ESSL Events Location 2013-2019'
+essl_analysis_functions.plot_events_paula([data_paula_red,data_paula_grey], encompassing_domain, [domain_red,domain_grey], False, raster_filename, title, path_file)
 
 
 #TODO add orography and plot the entire domain of EXPAT with highlith on the TEAMX domain
